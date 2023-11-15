e4b70f490302b69202c7aaf8e669fba0b6ea110e
dmitriy <dmitriy.churmeev@gmail.com>
2023-11-15 21:08:12 +0300

    init

---
+++ src/main/scala/App.scala
@@ -0,0 +1,52 @@
+import org.apache.spark.sql.catalyst.expressions.GenericRow
+import org.apache.spark.sql.expressions.MutableAggregationBuffer
+import org.apache.spark.sql.functions.{asc, col, count, desc, explode, lower, monotonically_increasing_id, split}
+import org.apache.spark.sql.types.{IntegerType, LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.{Column, DataFrame, Row, SparkSession}
+
+
+object App {
+
+  def main(args: Array[String]) = {
+
+    val spark = SparkSession.builder()
+      .appName("Spark_2_5_2")
+      .master("local")
+      .getOrCreate()
+
+    val subtitlesFrame1 = spark.read
+      .option("header","true")
+      .option("delimiter","|")
+      .option("inferSchema", "true")
+      .csv("src/main/resources/subtitles_s1.json")
+
+    val subtitlesFrame2 = spark.read
+      .option("header","true")
+      .option("delimiter","|")
+      .option("inferSchema", "true")
+      .csv("src/main/resources/subtitles_s2.json")
+
+    val countSubtitles1 = subtitlesFrame1.select(explode(split(lower(col("[{")), "\\W+")) as "w_s1").groupBy("w_s1")
+      .agg(count(col("w_s1")) as "cnt_s1")
+      .filter(col("w_s1") =!= "")
+      .sort(desc("cnt_s1"))
+      .withColumn("id", monotonically_increasing_id())
+      .limit(20)
+
+    val countSubtitles2 =  subtitlesFrame2.select(explode(split(lower(col("{")), "\\W+")) as "w_s2").groupBy("w_s2")
+      .agg(count(col("w_s2")) as "cnt_s2")
+      .filter(col("w_s2") =!= "")
+      .sort(desc("cnt_s2"))
+      .withColumn("id", monotonically_increasing_id())
+      .limit(20)
+
+    val innerJoinDF = countSubtitles1.col("id") === countSubtitles2.col("id")
+
+    countSubtitles1
+      .join(countSubtitles2, innerJoinDF,  "inner")
+      .drop(countSubtitles2.col("id"))
+      .select("w_s1","cnt_s1","id", "w_s2", "cnt_s2")
+      .show
+  }
+
+}
